1cc53df8619cdd958bdc894cc30d70fc15539e43
Siyam-Khan <siyam.sajeeb79@gmail.com>
2017-04-16 23:48:50 +0600

    business release : v2.1.1--16-4-17

--- 1-git-cmds-for-github.sh
+++ 1-git-cmds-for-github.sh
@@ -1,6 +1,6 @@
 git init
 git add .
-git commit -m "business release : v2.1--16-4-17"
+git commit -m "business release : v2.1.1--16-4-17"
 git remote add origin https://github.com/githubBangladesh/bookbucket
 git remote -v
 git push origin master
