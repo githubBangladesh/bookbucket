b93ffb858fefa4550ff379e915da81a6cc55b52f
Siyam-Khan <siyam.sajeeb79@gmail.com>
2017-04-16 23:14:06 +0600

    business release : v2.1--16-4-17

---
+++ terminal-git-github/1-git-cmds-for-github.sh
@@ -0,0 +1,6 @@
+git add .
+git commit -m "business release : v2--10-4-17"
+git remote add origin https://github.com/githubBangladesh/bookbucket
+git remote -v
+git push origin master
+git push --all -f https://github.com/githubBangladesh/bookbucket
